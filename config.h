33e733edfe1d3c7bbc372928ec2a62c0ffb4f43c
rkeepr <rkeepr@proton.me>
2023-04-08 19:44:15 -0300

    fix blank fonts in status bar with libertinus math

--- config.h
+++ config.h
@@ -5,10 +5,11 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10",
-					"Noto Sans CJK JP:size=10:antialias=true:autohint=true", 
+static const char *fonts[]          = {	"monospace:size=10:antialias=true:autohint=true",
+					"Noto Sans CJK JP:size=10:antialias=true:autohint=true",
 					"JoyPixels:pixelsize=12:antialias=true:autohint=true", 
-					"NotoColorEmoji:pixelsize=12:antialias=true:autohint=true" };
+					"NotoColorEmoji:pixelsize=12:antialias=true:autohint=true",
+					"Libertinus Math:size=10:antialias=true:autohint=true" };
 static const char col_gray1[]       = "#0e0e0e";    /* default #222222*/
 static const char col_gray2[]       = "#0e0e0e";    /* default #444444*/
 static const char col_gray3[]       = "#bbbbbb";    /* default #bbbbbb*/
